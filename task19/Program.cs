5c2362bc3035ce110b8597b09b1cca4638cec096
Serg <patron.serg@yandex.ru>
2023-05-02 21:23:40 +0300

    19h19

--- task19/Program.cs
+++ task19/Program.cs
@@ -8,4 +8,25 @@
 
 // 23432 -> да
 
+static bool IsPalindrome(int number)
+{
+    int originalNumber = number;
+    int reversedNumber = 0;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        reversedNumber = reversedNumber * 10 + digit; 
+        number /= 10;
+    }
+
+    return originalNumber == reversedNumber;
+}
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(IsPalindrome(number) ? "Число является палиндромом" : "Число не является палиндромом");
+
+
 

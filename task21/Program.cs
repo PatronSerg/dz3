5dd8368d5455a98e961088aded07a06706780bfb
Serg <patron.serg@yandex.ru>
2023-05-02 21:59:26 +0300

    21

--- task21/Program.cs
+++ task21/Program.cs
@@ -6,3 +6,23 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+double Rast(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt((Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2)) + (Math.Pow(z2 - z1, 2)));
+}
+
+
+Console.WriteLine("Введите Координату x1 для точки А");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Координату y1 для точки А");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Координату z1 для точки А");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Координату x2 для точки B");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Координату y2 для точки B");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Координату z2 для точки B");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Расстояние между двяму точками равно: {Math.Round(Rast(x1, y1, z1, x2, y2, z2), 3)}");

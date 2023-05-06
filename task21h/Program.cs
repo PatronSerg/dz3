705cf9145089c79de1afde1fb8248d2b3200f8fc
Serg <patron.serg@yandex.ru>
2023-05-06 20:33:22 +0300

    ffff

--- task21h/Program.cs
+++ task21h/Program.cs
@@ -4,22 +4,41 @@
 // между ними в N-мерном пространстве. Сначала задается N с клавиатуры, потом задаются координаты точек.
 
 
+
 Console.WriteLine("Задайте N-мерность пространства:");
 int N = Convert.ToInt32(Console.ReadLine());
 
 
-int[] Jopa = new int[N];
-int[] Pezda = new int[N];
+double[] Jopa = new double[N];
+double[] Pezda = new double[N];
 
 
 for (int i = 0; i < Jopa.Length; i++)
 {
-    Console.WriteLine($"Задайте {i+1} координату первой точки пространства:");
-    Jopa[i]=Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine($"Задайте {i+1} координату второй точки пространства:");
-    Pezda[i]=Convert.ToInt32(Console.ReadLine());
-    // Console.WriteLine(Jopa[i]);
+    Console.WriteLine($"Задайте {i + 1} координату первой точки пространства:");
+    Jopa[i] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Задайте {i + 1} координату второй точки пространства:");
+    Pezda[i] = Convert.ToInt32(Console.ReadLine());
 }
+Console.WriteLine($"Расстояние между двяму точками равно: {Math.Round(Rast(Jopa , Pezda), 3)}");
+
+
+
+double Rast(double[] Shlepa, double[] Clepa)
+{
+    double huesha = 0;
+
+    for (int i = 0; i < Clepa.Length; i++)
+    {
+    huesha = huesha + Math.Pow(Clepa[i] - Shlepa[i], 2);
+    }
+    return Math.Sqrt(huesha);
+    
+    //return Math.Sqrt((Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2)) + (Math.Pow(z2 - z1, 2)));
+}
+
+
+
 
 
 
